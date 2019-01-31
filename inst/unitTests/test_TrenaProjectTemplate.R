e3b9be0023d4afd2fb76077b75aff22b53d87e57
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-01-31 12:21:37 -0800

    tests fixes to getGene|EnhancersRegion

--- inst/unitTests/test_TrenaProjectTemplate.R
+++ inst/unitTests/test_TrenaProjectTemplate.R
@@ -63,6 +63,11 @@ test_expressionMatrices <- function()
 
 } # test_expressionMatrices
 #------------------------------------------------------------------------------------------------------------------------
+# setting the target gene implies a few other assignements, all tested here:
+#   geneInfo (temporarily also masquerading at tbl.transcripts
+#   geneRegion
+#   geneEnhancersRegion (when avaialable, defaults to geneRegion)
+#
 test_setTargetGene <- function()
 {
    message(sprintf("--- test_setTargetGene"))
@@ -73,21 +78,30 @@ test_setTargetGene <- function()
    message(sprintf("    transcripts"))
    tbl.transcripts <- getTranscriptsTable(tProj)
    checkTrue(nrow(tbl.transcripts) >= 1)
+   checkEquals(tbl.transcripts$chr, "chr4")
+   checkEquals(tbl.transcripts$start, 53030787)
+   checkEquals(tbl.transcripts$end , 53159895)
+   checkEquals(tbl.transcripts$tss, 53159895)
+   checkEquals(tbl.transcripts$strand, -1)
+
+   message(sprintf("    geneRegion"))
+   region <- getGeneRegion(tProj, flankingPercent=0)
+   checkTrue(all(c("chromLocString", "chrom", "start", "end") %in% names(region)))
+   checkEquals(region$chromLocString, "chr4:53030787-53159895")
 
    message(sprintf("    enhancers"))
    tbl.enhancers <- getEnhancers(tProj)
    checkEquals(colnames(tbl.enhancers), c("chrom", "start", "end", "type", "combinedScore", "geneSymbol"))
    checkTrue(nrow(tbl.enhancers) >= 0)
 
+   message(sprintf("    geneGeneEnhancersRegion"))
+   region <- getGeneEnhancersRegion(tProj, flankingPercent=0)
+   checkTrue(all(c("chromLocString", "chrom", "start", "end") %in% names(region)))
+   checkEquals(region$chromLocString, "chr4:52901678-53289004")
+
    message(sprintf("    encode DHS"))
    tbl.dhs <- getEncodeDHS(tProj)
    checkEquals(nrow(tbl.dhs), 0)
-
-   checkEquals(tbl.transcripts$chr, "chr4")
-   checkEquals(tbl.transcripts$start, 53030787)
-   checkEquals(tbl.transcripts$end , 53159895)
-   checkEquals(tbl.transcripts$tss, 53159895)
-   checkEquals(tbl.transcripts$strand, -1)
 
    message(sprintf("    ChIP-seq"))
    tbl.chipSeq <- getChipSeq(tProj, chrom=chromosome, start=start, end=end, tfs=NA)
